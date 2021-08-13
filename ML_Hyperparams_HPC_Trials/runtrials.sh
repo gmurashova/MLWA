e10921aee12af939f2f443eb2e3ecd5331aa725a
Dirk Colbry <colbyrdi@msu.edu>
2021-08-13 09:27:38 -0400

    Turning all files into a single program

---
+++ ML_Hyperparams_HPC_Trials/runtrials.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+./GM_ML_Histo_new_from_tutorial_op.py adadelta 0.0001
+./GM_ML_Histo_new_from_tutorial_op.py adadelta 0.001
+./GM_ML_Histo_new_from_tutorial_op.py adadelta 0.002
+./GM_ML_Histo_new_from_tutorial_op.py adadelta 0.01
+./GM_ML_Histo_new_from_tutorial_op.py adagradi 0.0001
+./GM_ML_Histo_new_from_tutorial_op.py adagrad 0.001
+./GM_ML_Histo_new_from_tutorial_op.py adagrad 0.002
+./GM_ML_Histo_new_from_tutorial_op.py adagrad 0.01
+./GM_ML_Histo_new_from_tutorial_op.py adamax 0.0001
+./GM_ML_Histo_new_from_tutorial_op.py adamax 0.001
+./GM_ML_Histo_new_from_tutorial_op.py adamax 0.002
+./GM_ML_Histo_new_from_tutorial_op.py adamax 0.01
+./GM_ML_Histo_new_from_tutorial_op.py adam 0.0001
+./GM_ML_Histo_new_from_tutorial_op.py adam 0.001
+./GM_ML_Histo_new_from_tutorial_op.py adam 0.002
+./GM_ML_Histo_new_from_tutorial_op.py adam 0.01
+./GM_ML_Histo_new_from_tutorial_op.py nadam 0.0001
+./GM_ML_Histo_new_from_tutorial_op.py nadam 0.001
+./GM_ML_Histo_new_from_tutorial_op.py nadam 0.002
+./GM_ML_Histo_new_from_tutorial_op.py nadam 0.01
+
+
