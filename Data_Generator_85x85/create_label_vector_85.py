ca34894b9255f067a226ad8f3321b7b63ba74609
gmurashova <89028372+gmurashova@users.noreply.github.com>
2021-08-16 12:31:07 -0400

    Update create_label_vector_85.py
    changed the pathname for data import and export.

--- Data_Generator_85x85/create_label_vector_85.py
+++ Data_Generator_85x85/create_label_vector_85.py
@@ -50,9 +50,9 @@ import glob
 import re
 print(tf.__version__)
 
-path1  = '/Users/gabrielleosborn-lipsitz/MOVE_BACK_TO_DESKTOP/ML_Research/TILES_85/CANCER'
-path2  = '/Users/gabrielleosborn-lipsitz/MOVE_BACK_TO_DESKTOP/ML_Research/TILES_85/NORMAL'
-path3  = '/Users/gabrielleosborn-lipsitz/MOVE_BACK_TO_DESKTOP/ML_Research/TILES_85/INFLAMMATION'
+path1  = '/TILES_85/CANCER'
+path2  = '/TILES_85/NORMAL'
+path3  = '/TILES_85/INFLAMMATION'
 
 pattern = re.compile(r'Cancer')
 pattern2 = re.compile(r'Normal')
