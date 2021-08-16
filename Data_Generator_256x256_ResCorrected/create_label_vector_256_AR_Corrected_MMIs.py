22ac0ffdc98d23df945318bf5cc6019b136355e2
gmurashova <89028372+gmurashova@users.noreply.github.com>
2021-08-16 12:27:13 -0400

    Update create_label_vector_256_AR_Corrected_MMIs.py
    changed the pathname for data import and export.

--- Data_Generator_256x256_ResCorrected/create_label_vector_256_AR_Corrected_MMIs.py
+++ Data_Generator_256x256_ResCorrected/create_label_vector_256_AR_Corrected_MMIs.py
@@ -50,9 +50,9 @@ import glob
 import re
 print(tf.__version__)
 
-path1  = '/Users/gabrielleosborn-lipsitz/MOVE_BACK_TO_DESKTOP/ML_Research/MMI_256x256_AR_Corrected/TILES_256_AR_Corrected/CANCER'
-path2  = '/Users/gabrielleosborn-lipsitz/MOVE_BACK_TO_DESKTOP/ML_Research/MMI_256x256_AR_Corrected/TILES_256_AR_Corrected/NORMAL'
-path3  = '/Users/gabrielleosborn-lipsitz/MOVE_BACK_TO_DESKTOP/ML_Research/MMI_256x256_AR_Corrected/TILES_256_AR_Corrected/INFLAMMATION'
+path1  = '/TILES_256_AR_Corrected/CANCER'
+path2  = '/TILES_256_AR_Corrected/NORMAL'
+path3  = '/TILES_256_AR_Corrected/INFLAMMATION'
 
 pattern = re.compile(r'Cancer')
 pattern2 = re.compile(r'Normal')
